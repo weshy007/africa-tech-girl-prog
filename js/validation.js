edcc2e4b8056fec4753bc02a23e1e7ee86567e7f
weshy <josephwaweru96@gmail.com>
2022-05-21 21:37:25 +0300

    form validation with js

---
+++ js/validation.js
@@ -0,0 +1,66 @@
+(function() {
+    'use strict'; //undeclared variables cannot be used
+    window.addEventListener('load', function() { //level 1 start
+      var form = document.getElementById('frmContact'); //receive the Contact form with id frmContact
+          form.addEventListener('submit', function(event) { //level 2 start
+          if (form.checkValidity() === false) { //execute lines of code within the if, if the form fails validation
+            event.preventDefault();//inbuilt fn
+            event.stopPropagation();//inbuilt fn
+            setValidationResponse();// a custom fn
+          } else { //execute lines of code in the else block, if the form passes validation
+            form.classList.add('was-validated');//add a class was-validated if the form passes validation
+            var userName = document.getElementById("userName").value; //initialize userName with the Name entered on the form
+            var upperUsername = userName.charAt(0).toUpperCase() + userName.slice(1) // capitalise first letter of a name
+
+            var userEmail = document.getElementById("userEmail").value;
+            var subject = document.getElementById("subject").value;
+            var content = document.getElementById("message").value;
+            alert("Here is what we got from you, "+ upperUsername + ".\nSubject: "+ subject + "\nEmail: " + userEmail + "\nMessage: "+content);
+            alert("Thank you "+upperUsername+". Your message has been sent."); //Give personalized Feedback
+          }
+        }, false); //level 2 end
+    }, false); //level 1 end
+  })(); //end of function 
+  
+  function setValidationResponse() {
+      var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;//Regular expression to validate email addresses
+      var userName = document.getElementById("userName").value;
+      var userEmail = document.getElementById("userEmail").value;
+      var subject = document.getElementById("subject").value;
+      var content = document.getElementById("message").value;
+      
+      if (userName == "") {
+          markAsInvalid("userName", "required");
+      } else {
+          markAsValid("userName");
+      }
+      
+      if (userEmail == "") {
+          markAsInvalid("userEmail", "required");
+      } else if(!emailRegex.test(userEmail)) {
+          markAsInvalid("userEmail", "invalid email");
+      } else {
+          markAsValid("userEmail");
+      }
+      
+      if (subject == "") {
+          markAsInvalid("subject", "required");
+      } else {
+          markAsValid("subject");
+      }
+      
+      if (content == "") {
+          markAsInvalid("content", "required");
+      } else {
+          markAsValid("content");
+      }
+  }
+  
+  function markAsValid(id) {
+      document.getElementById(id+"-info").style.display = "none";
+  }
+  
+  function markAsInvalid(id, feedback) {
+      document.getElementById(id+"-info").style.display = "inline";
+      document.getElementById(id+"-info").innerText = feedback;
+  }
